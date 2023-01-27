40c59129c11f314630033b0b0530d5746b155fca
Connie Wong <conniehhw@gmail.com>
2023-01-27 13:05:41 -0500

    Clean up code, add comments

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
  
-  # TEST TEST TEST  
+  # TEST  
 
-  [![License: MIT](https://imgshields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
-   
+  [![License: MIT](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://choosealicense.com/licenses/mit/)
   
+
   ## Description
-  TEST TEST TEST
-      
+  TEST
+  
+
+
+
   ## Table of Contents
   - [Installation](#installation)
   - [usage](#usage)
@@ -14,23 +17,52 @@
   - [Tests](#tests)
   - [License](#license)
   - [Questions](#questions)
-      
+ 
+
+
+
   ## Installation
   Click [here](https://drive.google.com/file/d/1X2eI7_ovsnll62cR3eTkquUjK4zZf8he/view) to see a walkthrough video.
-  TEST
-      
+  TES
+ 
+
+
+
+
+
   ## Usage
+  TEst
   
-      
+
+
+
+
+
   ## Contributing
-  
-      
+  test
+
+
+
+
+
+
   ## Tests
-  TESTTESTEST
-      
+  test
+ 
+
+
+
+
   ## License
   Licensed under the [MIT](https://choosealicense.com/licenses/mit/) license
-        
+
+  
+
+
+
   ## Questions
-  If you have additional questions about the repo, you can contact me directly at email.com. You can find more of my work at Conniehhw (https://github.com/).
-          
+  If you have additional questions about the repo, you can contact me directly at TSET. You can find more of my work at TEST (https://github.com/TEST).
+ 
+
+
+    
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,48 +1,13 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {
-//   const badges = {
-//     MIT: '[![License: MIT](https://imgshields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)',
-//     APACHE2: '['
-//     GPL3:
-//     BSD3: 
-//   }
-// }
-
-// if(answers.license === 'MIT') {
-//   return badges.MIT;
-//   console.log(badges.MIT)
-//   } else if (answers.license === 'APACHE 2.0') {
-//   return badges.APACHE2;
-//   } else if (answers.license === 'GPL 3.0') {
-//   return badges.GPL3;
-//   } else if (answers.licensse === 'BSD 3') {
-//   return badges.BSD3;
-//   } else {
-//   return ' ';
-//   };
-
-// // TODO: Create a function that returns the license link
-// // // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-// function generateMarkdown(answers) {
-//   return `# ${answers.projectname}` 
-// }
+// functions to render license badge, link and section
 
 class MarkDown {
 
   static renderLicenseBadge(license){
     const badges = {
-      MIT: '[![License: MIT](https://imgshields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)',
+      MIT: '[![License: MIT](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://choosealicense.com/licenses/mit/)',
       APACHE: '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://choosealicense.com/licenses/apache-2.0/)',
       GPL3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://choosealicense.com/licenses/gpl-3.0/)',
-      BSD3: '[!License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)(https://choosealicense.com/licenses/bsd-3-clause-clear/)'
+      BSD3: '[![License BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://choosealicense.com/licenses/bsd-3-clause-clear/)'
     }
       return badges[license]
   }
@@ -66,20 +31,19 @@ class MarkDown {
     }
   }
   
-  
+  // export content & prompt inputs to readme.md
   static generateMarkdown(answers) {
     return ` 
   # ${answers.projectname}  
 
   ${this.renderLicenseBadge(answers.license)}
-  \ 
-  \
+  
 
   ## Description
   ${answers.description}
-  \
-  \
-  \
+  
+
+
 
   ## Table of Contents
   - [Installation](#installation)
@@ -88,46 +52,54 @@ class MarkDown {
   - [Tests](#tests)
   - [License](#license)
   - [Questions](#questions)
-  \
-  \
-  \
+ 
+
+
 
   ## Installation
   Click [here](https://drive.google.com/file/d/1X2eI7_ovsnll62cR3eTkquUjK4zZf8he/view) to see a walkthrough video.
   ${answers.installation}
-  \
-  \
-  \
+ 
+
+
+
+
 
   ## Usage
   ${answers.usage}
-  \
-  \
-  \
+  
+
+
+
+
 
   ## Contributing
   ${answers.contribution}
-  \
-  \
-  \
+
+
+
+
+
 
   ## Tests
   ${answers.tests}
-  \
-  \
-  \
+ 
+
+
+
 
   ## License
   ${this.renderLicenseSection(answers.license)}
-  \
-  \
-  \
+
   
+
+
+
   ## Questions
-  If you have additional questions about the repo, you can contact me directly at ${answers.email}. You can find more of my work at ${answers.username} (https://github.com/).
-  \
-  \
-  \
+  If you have additional questions about the repo, you can contact me directly at ${answers.email}. You can find more of my work at ${answers.username} (https://github.com/${answers.username}).
+ 
+
+
     `
   }
 }
